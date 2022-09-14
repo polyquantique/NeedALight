7d683e2ed1e015959658410d10c97c013fbfb4bc
MHoude2 <martin.houde2@gmail.com>
2022-09-14 12:06:36 -0400

    init

---
+++ NeedALight/__init__.py
@@ -0,0 +1,10 @@
+from ._version import __version__
+
+
+def version():  # pragma: no cover
+    r"""
+    Get version number of sqtom
+    Returns:
+      str: The package version number
+    """
+    return __version__
